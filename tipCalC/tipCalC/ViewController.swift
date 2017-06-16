48c7da49b9f0f7fed112d1d02a37afc13f9bd653
Your Name <rintudhaya@gmail.com>
2017-06-16 15:41:17 -0400

    added multiple files

--- tipCalC/tipCalC/ViewController.swift
+++ tipCalC/tipCalC/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var AmountTextField: UITextField!
+    
+    @IBOutlet weak var TipAmountTextField: UITextField!
+    
+    @IBOutlet weak var TipOutlet: UISegmentedControl!
+    
+    @IBOutlet weak var totalAmountField: UITextField!
+    
+    @IBOutlet weak var gifview: UIImageView!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        gifview.loadGif(name: "dollar")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func clearButton(_ sender: Any) {
+        
+        AmountTextField.text = ""
+        TipAmountTextField.text = ""
+        totalAmountField.text = ""
+        
+        //TipPercentSelector(<#T##sender: UISegmentedControl##UISegmentedControl#>)
+        //UISegmentedControlSegment.hashValue = 0
+        TipOutlet.selectedSegmentIndex = -1
     }
-
-
+    
+    
+    @IBAction func TipPercentSelector(_ sender: UISegmentedControl) {
+        
+        if let billAmount = Double(AmountTextField.text!) {
+            switch TipOutlet.selectedSegmentIndex {
+            case 0:
+                TipAmountTextField.text = String((billAmount * 15.0) / 100.0)
+                
+            case 1:
+                TipAmountTextField.text = String((billAmount * 20.0) / 100.0)
+                
+            case 2:
+                TipAmountTextField.text = String((billAmount * 25.0) / 100.0)
+            default:
+                break
+            }
+        } else {
+            AmountTextField.text = ""
+        }
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        if AmountTextField.text != "" && TipAmountTextField.text != ""{
+            calculateSound()
+        }
+        if let amount = Double(AmountTextField.text!) , let tip = Double(TipAmountTextField.text!) {
+            let totalAmount = amount + tip
+            totalAmountField.text = String(totalAmount)
+        } else {
+            AmountTextField.text = ""
+        }
+        
+    }
+    
 }
 
--- tipCalC/tipCalC/sound.swift
+++ tipCalC/tipCalC/sound.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import AVFoundation
+
+
+
+var audioPlayer1 = AVAudioPlayer()
+func calculateSound() {
+    
+    let alertSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "coin", ofType: "mp3")!)
+    
+    do {
+        try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+    } catch{
+    }
+    do {
+        try AVAudioSession.sharedInstance().setActive(true)
+    } catch{
+    }
+    
+    // Play the sound
+    do {
+        audioPlayer1 = try AVAudioPlayer(contentsOf: alertSound as URL)
+    } catch {
+    }
+    
+    audioPlayer1.prepareToPlay()
+    audioPlayer1.play()
+}
